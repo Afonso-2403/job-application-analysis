9bb68156e9dd01b7344dd2fcffe290c177d52854
Afonso Luz <avsluz@gmail.com>
2026-02-15 13:54:54 +0100

    Improve needs follow-up logic and add analytics

--- google_sheets.py
+++ google_sheets.py
@@ -2,16 +2,14 @@ import gspread
 from pathlib import Path
 import pandas as pd
 
-SHEET_NAME = "Job_Application_Tracker"
 
-
-def get_spreadsheet_data():
+def get_spreadsheet_data(sheet_name: str):
     gc = gspread.oauth(
         credentials_filename=f"{Path(__file__).parent}/.config/credentials.json",
         authorized_user_filename=f"{Path(__file__).parent}/.config/authorized_user.json"
     )
 
-    sheet = gc.open(SHEET_NAME).sheet1
+    sheet = gc.open(sheet_name).sheet1
 
     # The records start from row 6 (0-indexed)
     # The header row is row 4
@@ -20,5 +18,5 @@ def get_spreadsheet_data():
     data = all_values[6:]
 
     df = pd.DataFrame(data, columns=headers)
-    
+
     return df
